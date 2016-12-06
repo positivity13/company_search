705b55ce76e388a0738be66ea771ee2fc86fd800
Mark Jones <mark@mmadvertising.co.uk>
2016-12-06 11:16:49 +0000

    fixing key not in dict error

--- web.py
+++ web.py
@@ -3,9 +3,6 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-# access_token = os.environ.get('ACCESS_TOKEN')
-# #create search object
-# s = Search(access_token=access_token)
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -14,18 +11,17 @@ app = Flask(__name__)
 def index():
 	details = None
 	profile = None
-	co_no = None
 	name = request.values.get('name')
 	co_no = request.values.get('cono')
-	if name == None:
-		pass
-	else:
+	if name != None:
 		details = company_by_number(name)
-	if co_no == None:
-		pass
-	else:
+	if co_no != None:
 		profile = company_profile(co_no)
-	return render_template('index.html', name=name, co_no=co_no, details=details, profile=profile)
+	return render_template('index.html', 
+							name=name, 
+							co_no=co_no, 
+							details=details, 
+							profile=profile)
 
 @app.route('/about')
 def about():
